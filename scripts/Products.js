9fb00df87e6cd18eebfda8e722ecb83f46c2ac08
Daniel Bennett <danielpsalmbennett@yahoo.com>
2023-08-08 14:13:24 -0500

    done

--- scripts/Products.js
+++ scripts/Products.js
@@ -2,11 +2,26 @@ import { getProducts } from "./database.js"
 
 const products = getProducts()
 
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.dataset.type === "product"){
+        window.alert(`${itemClicked.dataset.name} costs $${parseFloat(itemClicked.dataset.price)}`)
+    }});
+
+
+
 export const Products = () => {
-    let html = "<ul>`
+    let html = "<ul>"
 
     for (const product of products) {
-        html += `<li>${product.title}</li>`
+        html += `<li data-id = "${product.id}"
+                     data-name = "${product.name}"
+                     data-price = "${product.price}"
+                     data-type = "product"
+                    >${product.name}</li>`
     }
 
     html += "</ul>"
